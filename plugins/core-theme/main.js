57e1a41c66f9dc4e927b4f159430cb7b7677070b
Ittay Dror <ittay.dror@gmail.com>
2011-07-07 22:48:39 +0300

    set font-size according to size

--- plugins/core-theme/main.js
+++ plugins/core-theme/main.js
@@ -13,14 +13,18 @@ jQuery('document').ready(function($){
 		footer = $('<script type="text/x-jquery-tmpl"></script>').append($('.slydes-footer'))
 	
 	function resize() {
+		console.log("size: " + $('#slydes-content').width() + "," + $('#slydes-content').height())
 		var dimension = Slydes.ratio_43($('#slydes-content').width(),$('#slydes-content').height())
 		
 		frame.height(dimension.height)
 		frame.width(dimension.width)
+		$('body').css('font-size', dimension.height / 1000 + 'em')
 	
+		var position = function(element) {strip.css('left', -(element.slydes().index) * dimension.width)}
 		Slydes.presentation.slides.width(dimension.width).bind('current', function(other){
-			strip.css('left', -($(this).slydes().index) * dimension.width)
+			position($(this))
 		})
+		position(Slydes.presentation.current())
 	}
 
 	if (!frame.hasClass('slydes-preset')) {
